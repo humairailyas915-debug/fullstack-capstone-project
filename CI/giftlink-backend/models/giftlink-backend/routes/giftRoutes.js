7e030a96b2f09719e8c80ef60de971e60c713835
humairailyas915-debug <humairailyas915@gmail.com>
2026-09-11 12:05:57 -0700

    Create giftRoutes.js

---
+++ CI/giftlink-backend/models/giftlink-backend/routes/giftRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const connectToDatabase = require('../models/db');
+
+router.get('/', async (req, res) => {
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection("gifts");
+        const gifts = await collection.find({}).toArray();
+        res.json(gifts);
+    } catch (e) {
+        res.status(500).send("Error fetching gifts");
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection("gifts");
+        const gift = await collection.findOne({ id: req.params.id });
+        if (!gift) return res.status(404).send("Gift not found");
+        res.json(gift);
+    } catch (e) {
+        res.status(500).send("Error fetching gift details");
+    }
+});
+
+module.exports = router;
