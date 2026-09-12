f76a29fac237114552e64be4de42e681a85cb7b0
humairailyas915-debug <humairailyas915@gmail.com>
2026-09-12 08:27:21 -0700

    Update searchRoutes.js

--- sentiment/backend/routes/searchRoutes.js
+++ sentiment/backend/routes/searchRoutes.js
@@ -3,14 +3,18 @@ const router = express.Router();
 const connectToDatabase = require('../models/db');
 
 router.get('/', async (req, res) => {
-    const db = await connectToDatabase();
-    const collection = db.collection("gifts");
-    let query = {};
-    if (req.query.category) {
-        query.category = req.query.category;
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection("gifts");
+        let query = {};
+        if (req.query.category) {
+            query.category = req.query.category;
+        }
+        const gifts = await collection.find(query).toArray();
+        res.json(gifts);
+    } catch (e) {
+        res.status(500).send(e.message);
     }
-    const gifts = await collection.find(query).toArray();
-    res.json(gifts);
 });
 
 module.exports = router;
