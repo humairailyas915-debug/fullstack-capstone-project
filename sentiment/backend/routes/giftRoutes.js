3519de11682a9bd55cc97190fb226c956271c2f6
humairailyas915-debug <humairailyas915@gmail.com>
2026-09-12 08:26:06 -0700

    Update giftRoutes.js

--- sentiment/backend/routes/giftRoutes.js
+++ sentiment/backend/routes/giftRoutes.js
@@ -2,18 +2,31 @@ const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
 
+// Get all gifts
 router.get('/', async (req, res) => {
-    const db = await connectToDatabase();
-    const collection = db.collection("gifts");
-    const gifts = await collection.find({}).toArray();
-    res.json(gifts);
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection("gifts");
+        const gifts = await collection.find({}).toArray();
+        res.json(gifts);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
 });
 
+// Get gift by ID
 router.get('/:id', async (req, res) => {
-    const db = await connectToDatabase();
-    const collection = db.collection("gifts");
-    const gift = await collection.findOne({ _id: req.params.id });
-    res.json(gift);
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection("gifts");
+        const gift = await collection.findOne({ _id: req.params.id });
+        if (!gift) {
+            return res.status(404).send("Gift not found");
+        }
+        res.json(gift);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
 });
 
 module.exports = router;
